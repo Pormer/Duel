7854a2143e4c50316276f9a0c799da1462d21c5f
pig-08 <157554537+pig-08@users.noreply.github.com>
2024-10-30 00:10:33 +0900

    StatSO 플레이어로 이동

--- Assets/00.Work/SW/01.Scripts/Player/Health.cs
+++ Assets/00.Work/SW/01.Scripts/Player/Health.cs
@@ -6,30 +6,28 @@ using UnityEngine;
 public class Health : MonoBehaviour, IPlayerComponents
 {
     private Player _player;
-    private StatSO stat;
 
     public event Action OnDeadEvent;
     public event Action OnHitEvent;
     public void Initialize(Player player)
     {
         _player = player;
-
     }
 
     public void TakeDamage(int damage)
     {
         if (_player.IsOnBarrier)
         {
-            if(stat.barrierCount > 0)
+            if(_player.Stat.barrierCount > 0)
             {
-                stat.barrierCount--;
+                _player.Stat.barrierCount--;
                 _player.OnHitBarrier?.Invoke();
                 return;
             }
         }
-        stat.hp -= damage;
+        _player.Stat.hp -= damage;
 
-        if (stat.hp <= 0) OnDeadEvent?.Invoke();
+        if (_player.Stat.hp <= 0) OnDeadEvent?.Invoke();
         else OnHitEvent?.Invoke();
     }
 
--- Assets/00.Work/SW/01.Scripts/Player/StatSO.cs
+++ Assets/00.Work/SW/01.Scripts/Player/StatSO.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "SO/Data/Stat")]
-public class StatSO : ScriptableObject
+public class StatSO : ScriptableObject, IPlayerComponents
 {
     public int hp;
     public int barrierCount;
     public int damage;
     public int curBlletCount;
     public float cooltime;
+
+    public void Initialize(Player player)
+    {
+
+    }
 }
