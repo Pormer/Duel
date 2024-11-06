c20933af5e8015fcbc47358d22efe2724b470a1f
pormer <juwonst@gmail.com>
2024-11-06 16:43:33 +0900

    온라인 플레이와 로컬 플레이 나누기

--- Assets/00.Work/JW/01.Scripts/Core/SelectManager.cs
+++ Assets/00.Work/JW/01.Scripts/Core/SelectManager.cs
@@ -13,20 +13,36 @@ public class SelectManager : MonoBehaviour
 
     private void Awake()
     {
-        GameManager.Instance.OnGameStart.AddListener
-        (
-            () => PlayerGroup = FindObjectsByType<Player>(FindObjectsSortMode.None).ToList()
-        );
-        GameManager.Instance.OnGameStart.AddListener(StartGame);
+        if (GameManager.Instance.IsOnlinePlay)
+        {
+            GameManager.Instance.OnGameStart.AddListener
+            (
+                () => PlayerGroup = FindObjectsByType<Player>(FindObjectsSortMode.None).ToList()
+            );
+            GameManager.Instance.OnGameStart.AddListener(StartOnlineGameClientRpc);
+        }
+        else
+        {
+            PlayerInitialize();
+        }
         
-        PlayerGroup[0].GetComponent<NetworkObject>().Spawn();
     }
 
-    public void StartGame()
+    [ClientRpc]
+    public void StartOnlineGameClientRpc()
     {
         if (NetworkManager.Singleton.IsHost && PlayerGroup[1].GetComponent<NetworkObject>().IsOwner) PlayerGroup.Reverse();
-        if (NetworkManager.Singleton.IsClient) PlayerGroup[1].transform.position = new Vector3(4, 0);
+        if (NetworkManager.Singleton.IsClient)
+        {
+            PlayerGroup[1].transform.position = new Vector3(4, 0); 
+            PlayerGroup[1].transform.eulerAngles = new Vector3(0, 180, 0);
+        }
         
+        PlayerInitialize();
+    }
+
+    private void PlayerInitialize()
+    {
         for (int i = 0; i < PlayerGroup.Count; i++)
         {
             PlayerGroup[i].Initialize(selectPlayerData[i], selectGunData[i]);
