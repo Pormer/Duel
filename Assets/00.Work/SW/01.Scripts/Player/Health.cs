f0b3b45ac7845995d9470aa91bc2e5aab0f5b9f4
pig-08 <157554537+pig-08@users.noreply.github.com>
2024-11-18 23:24:50 +0900

    UI 설정

--- Assets/00.Work/SW/01.Scripts/Player/Health.cs
+++ Assets/00.Work/SW/01.Scripts/Player/Health.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.InputSystem.Controls;
 
 public class Health : MonoBehaviour, IPlayerComponents
 {
