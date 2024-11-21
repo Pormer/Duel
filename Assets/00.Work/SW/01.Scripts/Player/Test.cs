3fffcd823dabd66604633bec1efc28edb1ea5d98
pig-08 <157554537+pig-08@users.noreply.github.com>
2024-11-21 16:19:50 +0900

    DataUiTest

--- Assets/00.Work/SW/01.Scripts/Player/Test.cs
+++ Assets/00.Work/SW/01.Scripts/Player/Test.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     private GunDataUiSet dataUiSet;
-    [SerializeField] private  GunDataSO sO;
+    [SerializeField] private GunDataSO sO;
     [SerializeField] private GunDataSO gun;
     private void Start()
     {
